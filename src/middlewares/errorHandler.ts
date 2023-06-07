988e3b5f349db28511cf1498d84b2e710362c823
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-07 15:36:12 +0300

    Update errorHandler with Mongoose errors

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,19 +1,51 @@
-import { Response, NextFunction, Request } from "express";
+import { Error } from "mongoose";
+import colors from "colors";
 
-import { ErrResponse } from "../utils/errorResponse";
+import ErrorResponse from "../utils/errorResponse";
+
+// Types
+import { Next, Req, Res } from "../types";
+
+// Fub fact 😁:
+//      if you remove on of the argument below this middleware won't work
+//      Express will use the default error handler instead
 
 const errorHandler = (
-  err: Error | ErrResponse,
-  req: Request,
-  res: Response,
-  next: NextFunction
+  err: Error | ErrorResponse,
+  req: Req,
+  res: Res,
+  next: Next
 ) => {
-  const status = "statusCode" in err ? err.statusCode : 500;
+  let error = { ...err };
+  error.message = err.message;
+
+  console.log(colors.red(err.stack ?? ""));
+
+  // Mongoose wrong ID.
+  if (err.name === "CastError") {
+    error = new ErrorResponse(`Resource not found`, 404);
+  }
+
+  // Mongoose duplicate key.
+  if ("code" in err && err.code === 11000) {
+    error = new ErrorResponse("Duplicate field value entered", 400);
+  }
+
+  // Mongoose invalid values.
+  if (err.name === "ValidationError") {
+    const message = Object.values((err as Error.ValidationError).errors)
+      .map(value => value.message)
+      .join(", ");
+
+    error = new ErrorResponse(message, 400);
+  }
+
+  const status = "statusCode" in error ? error.statusCode : 500;
 
   res.status(status).json({
     success: false,
     data: null,
-    error: err.message,
+    error: error.message || "Internal Server Error",
   });
 };
 
