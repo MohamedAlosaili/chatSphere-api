0ef7e07e9871b1cc1bfb85d79420f66eb1ca19f9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-07 15:00:06 +0300

    Add advancedResults middleware
    advancedResults middleware helps to paginate and limit results
    to a default number or a number provided by the consumer, also,
    it added features like selecting certain fields and sorting by
    a specific field.

---
+++ src/middlewares/advancedResults.ts
@@ -0,0 +1,76 @@
+import { Query } from "mongoose";
+
+import asyncHandler from "./asyncHandler";
+
+const advancedResults = asyncHandler(async (req, res, next) => {
+  let {
+    select,
+    sort,
+    page: pag,
+    limit: lmt,
+    ...reqQuery
+  } = req.query as {
+    [key: string]: string;
+  };
+
+  if (!req.model) {
+    throw new Error("Internal Server Error");
+  }
+
+  // Add $ to MongoDB query operators
+  const filterStr = JSON.stringify(reqQuery).replace(
+    /\b(lt|lte|eq|gt|gte|in)\b/g,
+    str => `$${str}`
+  );
+
+  const filter = { ...JSON.parse(filterStr), ...req.filterQuery };
+
+  let query: Query<any, any> = req.model.find(filter);
+
+  if (req.populateQuery) query = query.populate(req.populateQuery);
+
+  // Select certain fields
+  if (select) {
+    const selectedFields = select.replace(/,/g, " ");
+    query = query.select(selectedFields);
+  }
+
+  // Sort results by a field
+  if (sort) {
+    query = query.sort(sort);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Paginate result
+  let page = Math.abs(parseInt(pag)) || 1;
+  let limit = Math.abs(parseInt(lmt)) || 25;
+
+  const startIndex = (page - 1) * limit; // 0
+  const endIndex = page * limit; // 25
+
+  const total =
+    (await req.model.countDocuments()) - (req.subtractFromTotal ?? 0);
+
+  query = query.skip(startIndex).limit(limit);
+
+  const results = await query;
+
+  const pagination = {
+    next: total > endIndex,
+    prev: startIndex > 0,
+    limit,
+    page,
+    pageSize: results.length,
+    totalPages: Math.ceil(total / limit),
+  };
+
+  res.json({
+    success: true,
+    data: results,
+    pagination,
+    total,
+  });
+});
+
+export default advancedResults;
