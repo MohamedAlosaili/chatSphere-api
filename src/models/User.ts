9e57d0d0e1357e4b13b436e7614f10c9f9c2e658
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-07 11:09:32 +0300

    Remove pre-save middleware and extract username from email as default value

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
 import mongoose, { ObjectId } from "mongoose";
 import regex from "../utils/regex";
 
-export interface User {
+export interface TUser {
   _id: string | ObjectId;
   username: string;
   email: string;
@@ -11,17 +11,21 @@ export interface User {
   createdAt: Date | string;
 }
 
-const UserSchema = new mongoose.Schema<User>(
+const UserSchema = new mongoose.Schema<TUser>(
   {
-    username: {
-      type: String,
-    },
     email: {
       type: String,
       required: true,
       match: regex.validEmail,
       unique: true,
       lowercase: true,
+      select: false,
+    },
+    username: {
+      type: String,
+      default: function () {
+        return this.email.split("@")[0];
+      },
     },
     isOnline: {
       type: Boolean,
@@ -42,13 +46,4 @@ const UserSchema = new mongoose.Schema<User>(
   }
 );
 
-UserSchema.pre("save", function (next) {
-  if (this.isNew && !this.username) {
-    const username = this.email.split("@")[0];
-
-    this.username = username;
-  }
-  next();
-});
-
-export default mongoose.model<User>("User", UserSchema);
+export default mongoose.model<TUser>("User", UserSchema);
