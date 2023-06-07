dea8ac7ed3aff6e4b649a626b8698978eaff4ce9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-07 15:39:24 +0300

    Update Req and Res additional type and added Next type
    Added aditional types to Req and Res types as optional

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,39 @@
-import { Request, Response } from "express";
-import { User } from "./models/User";
+import { NextFunction, Request, Response } from "express";
+import { TUser } from "./models/User";
+import { FilterQuery, Model } from "mongoose";
 
-export type Req = Request & { user?: User };
-export type Res = Response;
+export type Req = Request & Partial<AdditionalRequestProperties>;
+export type Res = Response & Partial<AdditionalResponseProperties>;
+export type Next = NextFunction;
+
+interface AdditionalRequestProperties {
+  user: TUser;
+  model: typeof Model;
+  filterQuery: FilterQuery<any>;
+  populateQuery: PopulateQuery[];
+  subtractFromTotal: number;
+}
+
+interface AdditionalResponseProperties {
+  advancedResults: AdvancedResults;
+}
+
+type PopulateQuery = {
+  path: string;
+  select?: string;
+  populate?: PopulateQuery[];
+};
+
+interface AdvancedResults {
+  success: boolean;
+  data: any;
+  pagination: {
+    next: boolean;
+    prev: boolean;
+    limit: number;
+    page: number;
+    pageSize: number;
+    totalPages: number;
+  };
+  total: number;
+}
