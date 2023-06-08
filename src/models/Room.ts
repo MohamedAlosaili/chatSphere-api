2f3a889c2084d339282f3b21f0fbb122c635fbf9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-08 17:57:20 +0300

    Remove timestamp from RoomSchema & add middleware to add owner if room created

--- src/models/Room.ts
+++ src/models/Room.ts
@@ -6,45 +6,64 @@ export interface TRoom {
   name: string;
   photo: string;
   roomOwner: string | ObjectId;
-  lastMessage: undefined; // TODO: Update after adding Message model
+  lastMessage: string | ObjectId; // TODO: Update after adding Message model
   moderators: string[];
+  updatedAt: Date | string; // To sort documents based on last message time
   createdAt: Date | string;
 }
 
-const RoomSchema = new mongoose.Schema<TRoom>(
-  {
-    private: {
-      type: Boolean,
-      default: false,
-    },
-    name: {
-      type: String,
-      required: [true, "Add room name"],
-    },
-    photo: {
-      type: String,
-      default: "default-room-photo.png",
-    },
-    roomOwner: {
-      type: mongoose.Types.ObjectId,
-      ref: "User",
-      requried: true,
-    },
-    lastMessage: {
-      type: mongoose.Types.ObjectId,
-      ref: "Message",
-      default: null,
-    },
-    moderators: [
-      {
-        type: mongoose.Types.ObjectId,
-        max: [5, "Moderators cannot be more than 5"],
-      },
-    ],
+const RoomSchema = new mongoose.Schema<TRoom>({
+  private: {
+    type: Boolean,
+    default: false,
   },
-  { timestamps: true }
-);
+  name: {
+    type: String,
+    required: [true, "Add room name"],
+  },
+  photo: {
+    type: String,
+    default: "default-room-photo.png",
+  },
+  roomOwner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    requried: true,
+  },
+  lastMessage: {
+    type: mongoose.Types.ObjectId,
+    ref: "Message",
+    default: null,
+  },
+  moderators: [
+    {
+      type: mongoose.Types.ObjectId,
+      max: [5, "Moderators cannot be more than 5"],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-// TODO: Add room owner as member pre save
+// Add room owner as a member on room creation
+RoomSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    try {
+      await this.$model("Member").create({
+        memberId: this.roomOwner,
+        roomId: this._id,
+      });
+    } catch (err) {
+      next(err as any);
+    }
+  }
+});
+
 // TODO: Cascade deleting related docs when the room removed
 export default mongoose.model("Room", RoomSchema);
