658368eff3c04400a68498568dfe37ce552236d9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-08 18:00:55 +0300

    Add Member model

---
+++ src/models/Member.ts
@@ -0,0 +1,29 @@
+import mongoose, { ObjectId } from "mongoose";
+
+export interface TMember {
+  _id: string | ObjectId;
+  memberId: string | ObjectId;
+  roomId: string | ObjectId;
+  createdAt: Date | string;
+}
+
+const MemberSchema = new mongoose.Schema<TMember>(
+  {
+    memberId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Add user id"],
+    },
+    roomId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Room",
+      required: [true, "Add room id"],
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+// Prevent adding member twice
+MemberSchema.index({ memberId: 1, roomId: 1 }, { unique: true });
+
+export default mongoose.model("Member", MemberSchema);
