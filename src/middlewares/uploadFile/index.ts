3f5775f017fc73ce2a6cc9d8c0ab859adbe4eee3
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-10 22:05:14 +0300

    Add uploadMessageFile to validate and upload message's file

--- src/middlewares/uploadFile/index.ts
+++ src/middlewares/uploadFile/index.ts
@@ -3,22 +3,25 @@ import path from "path";
 import asyncHandler from "../asyncHandler";
 import ErrorResponse from "../../utils/errorResponse";
 import uploadToStorage, { UploadedFile } from "../../utils/firebase";
-import { validateSize } from "./validation";
-import { User } from "../../models/User";
+import { getPhotoFilePath, validateSize, randomString } from "./utils";
 
 const uploadFile = asyncHandler(async (req, res, next) => {
   const file = req.file;
+
   if (!file) {
-    return next(new ErrorResponse("Invalid/Missing file", 400));
+    return next();
   }
 
   try {
     let uploadedFile: UploadedFile | null = null;
 
-    // TODO: Add media type ~ another file type
+    // TODO: Add file type ~ another file type
     switch (file.fieldname) {
       case "photo":
-        uploadedFile = await uploadPhoto(file, req.user as User);
+        uploadedFile = await uploadPhoto(file, getPhotoFilePath(req));
+        break;
+      case "file":
+        uploadedFile = await uploadMessageFile(file);
         break;
     }
 
@@ -30,7 +33,7 @@ const uploadFile = asyncHandler(async (req, res, next) => {
   }
 });
 
-const uploadPhoto = async (file: Express.Multer.File, user: User) => {
+const uploadPhoto = async (file: Express.Multer.File, filePath: string) => {
   const size = validateSize("image", file.size, file.fieldname);
   if (size.invalid) {
     throw new ErrorResponse(size.error ?? "Invalid Size", 400);
@@ -40,9 +43,26 @@ const uploadPhoto = async (file: Express.Multer.File, user: User) => {
     throw new ErrorResponse("Invalid file type, only image type", 400);
   }
 
-  const fileName = `${user._id}${path.extname(file.originalname)}`;
+  const fileName = `${filePath}${path.extname(file.originalname)}`;
 
   return await uploadToStorage(file, "photos", fileName);
+};
+
+const uploadMessageFile = async (file: Express.Multer.File) => {
+  const mediaRegex = /^(image|video).*/;
+
+  const mediaType = file.mimetype.substring(0, file.mimetype.indexOf("/"));
+
+  const fileType = mediaRegex.test(file.mimetype) ? mediaType : "file";
+
+  const size = validateSize(fileType, file.size, fileType);
+  if (size.invalid) {
+    throw new ErrorResponse(size.error ?? "Invalid Size", 400);
+  }
+
+  const fileName = `${randomString()}${path.extname(file.originalname)}`;
+
+  return await uploadToStorage(file, "files", fileName);
 };
 
 export default uploadFile;
