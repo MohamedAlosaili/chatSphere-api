2a8183531daad584bac75a7f25c05320a57ce9b9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-10 18:49:46 +0300

    Rename isRoomOnwer to checkRoomOwner middleware

--- src/middlewares/room.ts
+++ src/middlewares/room.ts
@@ -27,7 +27,7 @@ export const checkRoomExistence = asyncHandler(async (req, res, next) => {
 // @desc    Room can only modified by the owner
 // @route   PUT, POST, DELETE /api/rooms/:roomId/*
 // access   Private
-export const isRoomOwner = (req: Req, res: Res, next: Next) => {
+export const checkRoomOwner = (req: Req, res: Res, next: Next) => {
   if (req.user!._id.toString() !== req.room!.roomOwner.toString()) {
     return next(new ErrorResponse("Not authorized to modify this room", 403));
   }
