a16845575b4ff29f11ccb7f6819e5c7792d5a249
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-10 18:48:48 +0300

    Add left route

--- src/routes/members.ts
+++ src/routes/members.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getRoomMembers, joinRoom } from "../controllers/members";
+import { getRoomMembers, joinRoom, leftRoom } from "../controllers/members";
 
 import advancedResults from "../middlewares/advancedResults";
 
@@ -12,5 +12,7 @@ const router = express.Router({ mergeParams: true });
 router.route("/").get(getRoomMembers, advancedResults);
 
 router.post("/join", joinRoom);
+
+router.delete("/left", leftRoom);
 
 export default router;
