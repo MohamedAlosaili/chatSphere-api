f42cad5ff96bc56bd1987f5a5ef4a4feb647a6ec
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-12 13:53:16 +0300

    Add offline users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,10 @@
 import express from "express";
 
-import { getOnlineUsers, getUsers } from "../controllers/users";
+import {
+  getOfflineUsers,
+  getOnlineUsers,
+  getUsers,
+} from "../controllers/users";
 
 import advancedResults from "../middlewares/advancedResults";
 
@@ -8,5 +12,6 @@ const router = express.Router();
 
 router.get("/", getUsers, advancedResults);
 router.get("/online", getOnlineUsers, advancedResults);
+router.get("/offline", getOfflineUsers, advancedResults);
 
 export default router;
