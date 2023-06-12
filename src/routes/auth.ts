49b0b5e98d3121bd4107878b1d3c3611498348e3
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-12 17:36:36 +0300

    Update limit request on the login route to 5 per 1 hour

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -12,12 +12,21 @@ import {
 
 import protect from "../middlewares/protect";
 import uploadFile from "../middlewares/uploadFile/index";
+import limitRequest from "../lib/rateLimit";
 
 const router = express.Router();
 const updalod = multer();
 
 // Logout handled by nextjs server -> just removing the token from cookies and redirect
-router.post("/login", login);
+router.post(
+  "/login",
+  limitRequest(
+    60 * 60 * 1000,
+    5,
+    "Too many login attempts, try again after 1 hour"
+  ),
+  login
+);
 
 router.get("/currentUser", protect, currentUser);
 
