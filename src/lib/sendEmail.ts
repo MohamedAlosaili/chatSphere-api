aebe4fdf408d174c827ac99874f8cc0011219ae9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-19 23:05:14 +0300

    Update sendEmail html markup

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -13,7 +13,61 @@ export const sendLoginLink = async (email: string, url: string) => {
     from: "ChatSphere 💬 <info@chatsphere.com>",
     to: email,
     subject: "Sign in to ChatSphere 💬",
-    html: `<a href="${url}">Login</a>`,
-    // TODO: Update with proper HTML page
+    html: `
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ChatSphere Login email</title>
+  </head>
+  <body>
+    <div
+      style="
+        font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
+          'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif,
+          'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+          'Noto Color Emoji';
+        align-items: center;
+        max-width: 30rem;
+        margin: 0 auto;
+        text-align: center;
+      "
+    >
+      <h1 style>ChatSphere 💬</h1>
+
+      <h3>Log in as <strong>${email}</strong> to ChatSphere</h3>
+
+      <a
+        style="
+          font-size: 2rem;
+          padding: 1rem 2rem;
+          background-color: #000;
+          color: #fff;
+          border-radius: 0.5rem;
+          line-height: 1;
+          cursor: pointer;
+          display: block;
+          width: fit-content;
+          margin: 0.5rem auto;
+        "
+        href="${url}"
+        >Login</a
+      >
+
+      <p>
+        The link is valid for 1 hours. You will stay logged in for 30 days. If
+        you didn't request this link or you request it by mistake you can ignore
+        this email
+      </p>
+      <p style="align-self: flex-start">
+        Best regard, and enjoyable experience
+      </p>
+      <p style="align-self: flex-start">ChatSphere 💬 team.</p>
+    </div>
+  </body>
+</html>
+
+    `,
   });
 };
