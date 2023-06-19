90fc694275e10895423e082de4fe7316664697fd
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-06-19 23:06:35 +0300

    Add socket.io setup and listeners for 'new messages' & 'update online'

---
+++ src/socket.io.ts
@@ -0,0 +1,33 @@
+import { Server } from "http";
+import SocketIO from "socket.io";
+import Member from "./models/Member";
+
+const socketIO = (server: Server) => {
+  const io = new SocketIO.Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", socket => {
+    // console.log("User connected");
+    socket.on("new message", async (roomId: string) => {
+      io.emit(`room-${roomId}`);
+
+      const members = await Member.find({ roomId });
+      const memberIds = members.map(member => String(member.memberId));
+
+      memberIds.forEach(id => io.emit(id));
+    });
+
+    socket.on("update online", () => {
+      socket.broadcast.emit("update online");
+    });
+
+    socket.on("disconnect", () => {
+      // console.log("user disconnected");
+    });
+  });
+};
+
+export default socketIO;
